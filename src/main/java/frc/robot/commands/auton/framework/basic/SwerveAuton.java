b5eaf82383dc5bd74a9a28f241c2e57087e4142d
Trevor Sears <trevor@trevorsears.com>
2024-02-23 12:28:12 -0500

    Fixed indentation and basic formatting in `SwerveAuton.java`.

--- src/main/java/frc/robot/commands/auton/framework/basic/SwerveAuton.java
+++ src/main/java/frc/robot/commands/auton/framework/basic/SwerveAuton.java
@@ -11,47 +11,62 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.swerve.Swerve;
 
 public class SwerveAuton extends Command {
-  
-  Swerve swerveSubsystem;
-  double xSpeed, ySpeed, desiredRotation;
-  PIDController rotationalPID;
-  Timer timer;
-
-  public SwerveAuton(Swerve swerveSubsystem, double xSpeed, double ySpeed, double desiredRotation) {
-    this.swerveSubsystem = swerveSubsystem;
-    this.xSpeed = xSpeed;
-    this.ySpeed = ySpeed;
-    this.desiredRotation = desiredRotation;
-    this.timer = new Timer();
-    this.rotationalPID = new PIDController(.01, 0, 0);
-    rotationalPID.enableContinuousInput(0, 360);
-    addRequirements(swerveSubsystem);
-  }
-
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-    swerveSubsystem.stop();
-    timer.restart();
-  }
-
-  double thetaSpeed;
-  @Override
-  public void execute() {
-    thetaSpeed = rotationalPID.calculate(swerveSubsystem.getGyroRotation().getDegrees(), desiredRotation);
-    swerveSubsystem.updateModules(new ChassisSpeeds(xSpeed, ySpeed, desiredRotation), 1);
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    swerveSubsystem.stop();
-    timer.stop();
-  }
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return timer.hasElapsed(5);
-  }
+	
+	Swerve swerveSubsystem;
+	
+	double xSpeed, ySpeed, desiredRotation;
+	
+	PIDController rotationalPID;
+	
+	Timer timer;
+	
+	public SwerveAuton(Swerve swerveSubsystem, double xSpeed, double ySpeed, double desiredRotation) {
+		
+		this.swerveSubsystem = swerveSubsystem;
+		this.xSpeed = xSpeed;
+		this.ySpeed = ySpeed;
+		this.desiredRotation = desiredRotation;
+		this.timer = new Timer();
+		this.rotationalPID = new PIDController(.01, 0, 0);
+		rotationalPID.enableContinuousInput(0, 360);
+		addRequirements(swerveSubsystem);
+		
+	}
+	
+	// Called when the command is initially scheduled.
+	@Override
+	public void initialize() {
+		
+		swerveSubsystem.stop();
+		timer.restart();
+		
+	}
+	
+	double thetaSpeed;
+	
+	@Override
+	public void execute() {
+		
+		thetaSpeed = rotationalPID.calculate(swerveSubsystem.getGyroRotation().getDegrees(), desiredRotation);
+		swerveSubsystem.updateModules(new ChassisSpeeds(xSpeed, ySpeed, desiredRotation), 1);
+		
+	}
+	
+	// Called once the command ends or is interrupted.
+	@Override
+	public void end(boolean interrupted) {
+		
+		swerveSubsystem.stop();
+		timer.stop();
+		
+	}
+	
+	// Returns true when the command should end.
+	@Override
+	public boolean isFinished() {
+		
+		return timer.hasElapsed(5);
+		
+	}
+	
 }
