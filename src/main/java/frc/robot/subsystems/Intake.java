d11d2e6a50a656c6e4fc02ca4033a0a51a146352
Trevor Sears <trevor@trevorsears.com>
2024-02-23 12:03:25 -0500

    Fixed indentation and basic formatting in `Intake.java`.

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -11,48 +11,63 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
-  
-  CANSparkMax leftIntakeMotor, rightIntakeMotor;
-
-  public Intake(int intakeMotorLeftID, int intakeMotorRightID) {
-    leftIntakeMotor = new CANSparkMax(intakeMotorLeftID, MotorType.kBrushless);
-    rightIntakeMotor = new CANSparkMax(intakeMotorRightID, MotorType.kBrushless);
-    rightIntakeMotor.setInverted(true);
-    leftIntakeMotor.setInverted(false);
-    rightIntakeMotor.setIdleMode(IdleMode.kBrake);
-    leftIntakeMotor.setIdleMode(IdleMode.kBrake);
-  }
-
-  public void stop () {
-    leftIntakeMotor.stopMotor();
-    rightIntakeMotor.stopMotor();
-  }
-
-  double intakeMotorSpeed = .9;
-  int speedMultiplier = 1;
-  public void runIntake (boolean reverseMode) {
-    if (reverseMode) speedMultiplier = 1;
-    else speedMultiplier = -1;
-    leftIntakeMotor.set(speedMultiplier * intakeMotorSpeed);
-    rightIntakeMotor.set(speedMultiplier * intakeMotorSpeed);
-  }
-
-  public void increaseIntakeSpeed () {
-    if (intakeMotorSpeed <= 1)
-    intakeMotorSpeed += .05;
-  }
-
-  public void decreaseIntakeSpeed () {
-    if (intakeMotorSpeed >= -1)
-    intakeMotorSpeed -= .05;
-  }
-
-  public boolean isHoldingNote () {
-    return false;
-  }
-
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+    
+    CANSparkMax leftIntakeMotor, rightIntakeMotor;
+    
+    public Intake(int intakeMotorLeftID, int intakeMotorRightID) {
+		
+        leftIntakeMotor = new CANSparkMax(intakeMotorLeftID, MotorType.kBrushless);
+        rightIntakeMotor = new CANSparkMax(intakeMotorRightID, MotorType.kBrushless);
+        rightIntakeMotor.setInverted(true);
+        leftIntakeMotor.setInverted(false);
+        rightIntakeMotor.setIdleMode(IdleMode.kBrake);
+        leftIntakeMotor.setIdleMode(IdleMode.kBrake);
+		
+    }
+    
+    public void stop () {
+		
+        leftIntakeMotor.stopMotor();
+        rightIntakeMotor.stopMotor();
+		
+    }
+    
+    double intakeMotorSpeed = .9;
+    int speedMultiplier = 1;
+	
+    public void runIntake (boolean reverseMode) {
+		
+        if (reverseMode) speedMultiplier = 1;
+        else speedMultiplier = -1;
+		
+        leftIntakeMotor.set(speedMultiplier * intakeMotorSpeed);
+        rightIntakeMotor.set(speedMultiplier * intakeMotorSpeed);
+		
+    }
+    
+    public void increaseIntakeSpeed () {
+		
+        if (intakeMotorSpeed <= 1) intakeMotorSpeed += .05;
+		
+    }
+    
+    public void decreaseIntakeSpeed () {
+		
+        if (intakeMotorSpeed >= -1) intakeMotorSpeed -= .05;
+	
+    }
+    
+    public boolean isHoldingNote () {
+		
+        return false;
+		
+    }
+    
+    @Override
+    public void periodic() {
+		
+        // This method will be called once per scheduler run
+		
+    }
+	
 }
