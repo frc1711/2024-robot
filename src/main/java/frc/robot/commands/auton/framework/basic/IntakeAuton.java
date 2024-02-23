75fd9b8fb93e58d466e64df14cd7b44aaabb8787
Trevor Sears <trevor@trevorsears.com>
2024-02-23 12:30:10 -0500

    Fixed indentation and basic formatting in `IntakeAuton.java`.

--- src/main/java/frc/robot/commands/auton/framework/basic/IntakeAuton.java
+++ src/main/java/frc/robot/commands/auton/framework/basic/IntakeAuton.java
@@ -8,35 +8,46 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Intake;
 
 public class IntakeAuton extends Command {
-  
-  Intake intakeSubsystem;
-
-  public IntakeAuton(Intake intakeSubsystem) {
-    this.intakeSubsystem = intakeSubsystem;
-    addRequirements(intakeSubsystem);
-  }
-
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-    intakeSubsystem.stop();
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    intakeSubsystem.runIntake(false);
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    intakeSubsystem.stop();
-  }
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return intakeSubsystem.isHoldingNote();
-  }
+	
+	Intake intakeSubsystem;
+	
+	public IntakeAuton(Intake intakeSubsystem) {
+		
+		this.intakeSubsystem = intakeSubsystem;
+		addRequirements(intakeSubsystem);
+		
+	}
+	
+	// Called when the command is initially scheduled.
+	@Override
+	public void initialize() {
+		
+		intakeSubsystem.stop();
+		
+	}
+	
+	// Called every time the scheduler runs while the command is scheduled.
+	@Override
+	public void execute() {
+		
+		intakeSubsystem.runIntake(false);
+		
+	}
+	
+	// Called once the command ends or is interrupted.
+	@Override
+	public void end(boolean interrupted) {
+		
+		intakeSubsystem.stop();
+		
+	}
+	
+	// Returns true when the command should end.
+	@Override
+	public boolean isFinished() {
+		
+		return intakeSubsystem.isHoldingNote();
+		
+	}
+	
 }
