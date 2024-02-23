9eba280c0576ff092cb9265a6b2fd8bfea4d309a
Trevor Sears <trevor@trevorsears.com>
2024-02-23 12:29:51 -0500

    Fixed indentation and basic formatting in `ArmAuton.java`.

--- src/main/java/frc/robot/commands/auton/framework/basic/ArmAuton.java
+++ src/main/java/frc/robot/commands/auton/framework/basic/ArmAuton.java
@@ -8,41 +8,54 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Arm;
 
 public class ArmAuton extends Command {
-  
-  Arm armSubsystem;
-  double angleInDegrees;
-
-  double acceptableError = .1;
-
-  public ArmAuton(Arm armSubsystem, double angleInDegrees) {
-    this.armSubsystem = armSubsystem;
-    this.angleInDegrees = angleInDegrees;
-    addRequirements(armSubsystem);
-  }
-
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-    armSubsystem.stop();
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    armSubsystem.setToAngle(angleInDegrees);
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    armSubsystem.stop();
-  }
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    if ((angleInDegrees - acceptableError) <= armSubsystem.getAngleDegrees() && armSubsystem.getAngleDegrees() <= (angleInDegrees + acceptableError))
-    return true;
-    else return false;
-  }
+	
+	Arm armSubsystem;
+	
+	double angleInDegrees;
+	
+	double acceptableError = .1;
+	
+	public ArmAuton(Arm armSubsystem, double angleInDegrees) {
+		
+		this.armSubsystem = armSubsystem;
+		this.angleInDegrees = angleInDegrees;
+		addRequirements(armSubsystem);
+		
+	}
+	
+	// Called when the command is initially scheduled.
+	@Override
+	public void initialize() {
+		
+		armSubsystem.stop();
+		
+	}
+	
+	// Called every time the scheduler runs while the command is scheduled.
+	@Override
+	public void execute() {
+		
+		armSubsystem.setToAngle(angleInDegrees);
+		
+	}
+	
+	// Called once the command ends or is interrupted.
+	@Override
+	public void end(boolean interrupted) {
+		
+		armSubsystem.stop();
+		
+	}
+	
+	// Returns true when the command should end.
+	@Override
+	public boolean isFinished() {
+		
+		return (
+			(angleInDegrees - acceptableError) <= armSubsystem.getAngleDegrees() &&
+			armSubsystem.getAngleDegrees() <= (angleInDegrees + acceptableError)
+		);
+		
+	}
+	
 }
