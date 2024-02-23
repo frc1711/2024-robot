cdcb29aaebd6ea1f5415cdec78e0f6e44db53c94
Trevor Sears <trevor@trevorsears.com>
2024-02-23 12:04:46 -0500

    Fixed indentation and basic formatting in `Arm.java`.

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -19,48 +19,68 @@ import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Arm extends SubsystemBase {
-  
-  CANSparkMax leftTuffbox, rightTuffbox;
-
-  PIDController anglePID;
-
-  SparkAbsoluteEncoder boreEncoder;
-
-  public Arm(int leftTuffboxID, int rightTuffboxID) {
-    leftTuffbox = new CANSparkMax(leftTuffboxID, MotorType.kBrushless);
-    rightTuffbox = new CANSparkMax(rightTuffboxID, MotorType.kBrushless); 
-    // boreEncoder = rightTuffbox.getAbsoluteEncoder(Type.kDutyCycle);
-    // leftTuffbox.setIdleMode(IdleMode.kBrake);
-    // rightTuffbox.setIdleMode(IdleMode.kBrake);
-    // rightTuffbox.setInverted(true); //Set the motor to be inverted so that it rotates in the same direction as the otehr motor with the same input value.
-    anglePID = new PIDController(.01, 0, 0);
-  }
-
-  public void stop() {
-    leftTuffbox.stopMotor();
-    rightTuffbox.stopMotor();
-  }
-
-  public double getAngleDegrees () {
-    return boreEncoder.getPosition() - boreEncoder.getZeroOffset();
-  }
-
-  double rotationSpeed, motorSpeeds;
-  public void setToAngle (double angleInDegrees) {
-    rotationSpeed = anglePID.calculate(getAngleDegrees(), angleInDegrees);
-    motorSpeeds = rotationSpeed / 2;
-
-    leftTuffbox.set(motorSpeeds);
-    rightTuffbox.set(motorSpeeds);
-  }
-
-  public void changeAngle (double speed) {
-    leftTuffbox.set(speed);
-    rightTuffbox.set(speed);
-  }
-
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+	
+	CANSparkMax leftTuffbox, rightTuffbox;
+	
+	PIDController anglePID;
+	
+	SparkAbsoluteEncoder boreEncoder;
+	
+	public Arm(int leftTuffboxID, int rightTuffboxID) {
+		
+		leftTuffbox = new CANSparkMax(leftTuffboxID, MotorType.kBrushless);
+		rightTuffbox = new CANSparkMax(rightTuffboxID, MotorType.kBrushless);
+		
+		// boreEncoder = rightTuffbox.getAbsoluteEncoder(Type.kDutyCycle);
+		
+		// leftTuffbox.setIdleMode(IdleMode.kBrake);
+		// rightTuffbox.setIdleMode(IdleMode.kBrake);
+		
+		// Set the motor to be inverted so that it rotates in the same direction
+		// as the other motor with the same input value.
+		// rightTuffbox.setInverted(true);
+		
+		anglePID = new PIDController(.01, 0, 0);
+		
+	}
+	
+	public void stop() {
+		
+		leftTuffbox.stopMotor();
+		rightTuffbox.stopMotor();
+		
+	}
+	
+	public double getAngleDegrees() {
+		
+		return boreEncoder.getPosition() - boreEncoder.getZeroOffset();
+        
+	}
+	
+	double rotationSpeed, motorSpeeds;
+	
+	public void setToAngle(double angleInDegrees) {
+		
+		rotationSpeed = anglePID.calculate(getAngleDegrees(), angleInDegrees);
+		motorSpeeds = rotationSpeed / 2;
+		
+		leftTuffbox.set(motorSpeeds);
+		rightTuffbox.set(motorSpeeds);
+        
+	}
+	
+	public void changeAngle(double speed) {
+		
+		leftTuffbox.set(speed);
+		rightTuffbox.set(speed);
+        
+	}
+	
+	@Override
+	public void periodic() {
+        
+		// This method will be called once per scheduler run
+        
+	}
+	
 }
