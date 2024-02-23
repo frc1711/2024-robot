509e6c802a4d71334ea311ed2f457436817ca1e7
Trevor Sears <trevor@trevorsears.com>
2024-02-23 12:27:44 -0500

    Fixed indentation and basic formatting in `ShooterAuton.java`.

--- src/main/java/frc/robot/commands/auton/framework/basic/ShooterAuton.java
+++ src/main/java/frc/robot/commands/auton/framework/basic/ShooterAuton.java
@@ -9,39 +9,51 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Shooter;
 
 public class ShooterAuton extends Command {
-  
-  Shooter shooterSubsystem;
-  Timer timer;
-
-  public ShooterAuton(Shooter shooterSubsystem) {
-    this.shooterSubsystem = shooterSubsystem;
-    this.timer = new Timer();
-    addRequirements(shooterSubsystem);
-  }
-
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-    shooterSubsystem.stop();
-    timer.restart();
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    shooterSubsystem.runShooter(1, 1);
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    shooterSubsystem.stop();
-    timer.stop();
-  }
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return timer.hasElapsed(2);
-  }
+	
+	Shooter shooterSubsystem;
+	
+	Timer timer;
+	
+	public ShooterAuton(Shooter shooterSubsystem) {
+		
+		this.shooterSubsystem = shooterSubsystem;
+		this.timer = new Timer();
+		addRequirements(shooterSubsystem);
+		
+	}
+	
+	// Called when the command is initially scheduled.
+	@Override
+	public void initialize() {
+		
+		shooterSubsystem.stop();
+		timer.restart();
+		
+	}
+	
+	// Called every time the scheduler runs while the command is scheduled.
+	@Override
+	public void execute() {
+		
+		shooterSubsystem.runShooter(1, 1);
+		
+	}
+	
+	// Called once the command ends or is interrupted.
+	@Override
+	public void end(boolean interrupted) {
+		
+		shooterSubsystem.stop();
+		timer.stop();
+		
+	}
+	
+	// Returns true when the command should end.
+	@Override
+	public boolean isFinished() {
+		
+		return timer.hasElapsed(2);
+		
+	}
+	
 }
