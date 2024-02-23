506c72854e291e7e068ed369a4a8cb4da2e55dfc
Trevor Sears <trevor@trevorsears.com>
2024-02-23 12:26:16 -0500

    Fixed indentation and basic formatting in `OdemetryAuton.java`.

--- src/main/java/frc/robot/commands/auton/framework/basic/OdometryAuton.java
+++ src/main/java/frc/robot/commands/auton/framework/basic/OdometryAuton.java
@@ -13,54 +13,71 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.swerve.Swerve;
 
 public class OdometryAuton extends Command {
-
-  // Odometry kinematics;
-  Swerve swerveDrive;
-  Pose2d targetPose;
-  PIDController xDistancePID, yDistancePID;
-  double speedMultiplier;
-  
-  public OdometryAuton(Swerve swerveDrive, Pose2d targetPose, double speedMultiplier) {
-    this.swerveDrive = swerveDrive;
-    this.targetPose = targetPose;
-    this.speedMultiplier = speedMultiplier;
-    xDistancePID = new PIDController(1, 0, 0);
-    yDistancePID = new PIDController(.01, 0, 0);
-    yDistancePID.enableContinuousInput(0, 360);
-    yDistancePID.setTolerance(1);
-    xDistancePID.setTolerance(.1);
-
-    addRequirements(swerveDrive);
-  }
-
-  @Override
-  public void initialize() {
-    swerveDrive.stop();
-  }
-
-  double displacementX, displacementY, speedX, speedY, speedTheta;
-  Pose2d robotPose;
-  @Override
-  public void execute() {
-    robotPose = swerveDrive.updateOdometry();
-
-    displacementX = robotPose.getX();
-    displacementY = robotPose.getY();
-
-    speedX = xDistancePID.calculate(displacementX, targetPose.getX());
-    speedY = xDistancePID.calculate(displacementY, targetPose.getY());
-    speedTheta = yDistancePID.calculate(swerveDrive.getGyroRotation().getDegrees(), targetPose.getRotation().getDegrees());
-
-    swerveDrive.updateModules(ChassisSpeeds.fromFieldRelativeSpeeds(speedX, speedY, speedTheta, swerveDrive.getGyroRotation()), speedMultiplier);
-  }
-
-  @Override
-  public void end(boolean interrupted) {
-    swerveDrive.stop();
-  }
-
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+	
+	// Odometry kinematics;
+	
+	Swerve swerveDrive;
+	
+	Pose2d targetPose;
+	
+	PIDController xDistancePID, yDistancePID;
+	
+	double speedMultiplier;
+	
+	public OdometryAuton(Swerve swerveDrive, Pose2d targetPose, double speedMultiplier) {
+		
+		this.swerveDrive = swerveDrive;
+		this.targetPose = targetPose;
+		this.speedMultiplier = speedMultiplier;
+		xDistancePID = new PIDController(1, 0, 0);
+		yDistancePID = new PIDController(.01, 0, 0);
+		yDistancePID.enableContinuousInput(0, 360);
+		yDistancePID.setTolerance(1);
+		xDistancePID.setTolerance(.1);
+		
+		addRequirements(swerveDrive);
+		
+	}
+	
+	@Override
+	public void initialize() {
+		
+		swerveDrive.stop();
+		
+	}
+	
+	double displacementX, displacementY, speedX, speedY, speedTheta;
+	
+	Pose2d robotPose;
+	
+	@Override
+	public void execute() {
+		
+		robotPose = swerveDrive.updateOdometry();
+		
+		displacementX = robotPose.getX();
+		displacementY = robotPose.getY();
+		
+		speedX = xDistancePID.calculate(displacementX, targetPose.getX());
+		speedY = xDistancePID.calculate(displacementY, targetPose.getY());
+		speedTheta = yDistancePID.calculate(swerveDrive.getGyroRotation().getDegrees(), targetPose.getRotation().getDegrees());
+		
+		swerveDrive.updateModules(ChassisSpeeds.fromFieldRelativeSpeeds(speedX, speedY, speedTheta, swerveDrive.getGyroRotation()), speedMultiplier);
+		
+	}
+	
+	@Override
+	public void end(boolean interrupted) {
+		
+		swerveDrive.stop();
+		
+	}
+	
+	@Override
+	public boolean isFinished() {
+		
+		return false;
+		
+	}
+	
 }
