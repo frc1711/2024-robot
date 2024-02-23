0bbeaa7ba69c445fdf1520ab2bbad26ae08ea5aa
Trevor Sears <trevor@trevorsears.com>
2024-02-23 12:02:05 -0500

    Fixed indentation and basic formatting in `Shooter.java`.

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -12,52 +12,72 @@ import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
-  
-  CANSparkMax leftShooterMotor, rightShooterMotor;
-
-  public Shooter(int leftShooterID, int rightShooterID) {
-    leftShooterMotor = new CANSparkMax(rightShooterID, MotorType.kBrushless);
-    rightShooterMotor = new CANSparkMax(leftShooterID, MotorType.kBrushless);
-    rightShooterMotor.setInverted(false);
-    leftShooterMotor.setInverted(true);
-    leftShooterMotor.setIdleMode(IdleMode.kCoast);
-    rightShooterMotor.setIdleMode(IdleMode.kCoast);
-  }
-
-  public  void stop () {
-    leftShooterMotor.stopMotor();
-    rightShooterMotor.stopMotor();
-  }
-
-  public void runShooter (double leftMotorSpeed, double rightMotorSpeed) {
-    leftShooterMotor.set(leftMotorSpeed);
-    rightShooterMotor.set(rightMotorSpeed);
-  }
-
-  double leftSpeed = .5, rightSpeed = .5;
-  public void runShooterTest () {
-    leftShooterMotor.set(leftSpeed);
-    rightShooterMotor.set(rightSpeed);
-  }
-
-  public void increaseShooterSpeed () {
-    if (leftSpeed <= 1) leftSpeed += .05;
-    if (rightSpeed <= 1) rightSpeed += .05;
-  }
-
-  public void decreaseShooterSpeed () {
-    if (leftSpeed >= -1) leftSpeed -= .05;
-    if (rightSpeed >= -1) rightSpeed -= .05;
-  }
-
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
-
-  @Override
-  public void initSendable (SendableBuilder builder) {
-    builder.addDoubleProperty("Left Motor Speed", () -> leftSpeed, null);
-    builder.addDoubleProperty("Right Motor Speed", () -> rightSpeed, null);
-  }
+	
+	CANSparkMax leftShooterMotor, rightShooterMotor;
+	
+	public Shooter(int leftShooterID, int rightShooterID) {
+		
+        leftShooterMotor = new CANSparkMax(rightShooterID, MotorType.kBrushless);
+        rightShooterMotor = new CANSparkMax(leftShooterID, MotorType.kBrushless);
+		
+        rightShooterMotor.setInverted(false);
+        leftShooterMotor.setInverted(true);
+		
+        leftShooterMotor.setIdleMode(IdleMode.kCoast);
+        rightShooterMotor.setIdleMode(IdleMode.kCoast);
+		
+    }
+    
+    public void stop () {
+		
+        leftShooterMotor.stopMotor();
+        rightShooterMotor.stopMotor();
+		
+    }
+    
+    public void runShooter (double leftMotorSpeed, double rightMotorSpeed) {
+		
+        leftShooterMotor.set(leftMotorSpeed);
+        rightShooterMotor.set(rightMotorSpeed);
+		
+    }
+        
+    double leftSpeed = .5, rightSpeed = .5;
+	
+    public void runShooterTest () {
+		
+        leftShooterMotor.set(leftSpeed);
+        rightShooterMotor.set(rightSpeed);
+		
+    }
+    
+    public void increaseShooterSpeed () {
+		
+        if (leftSpeed <= 1) leftSpeed += .05;
+        if (rightSpeed <= 1) rightSpeed += .05;
+		
+    }
+    
+    public void decreaseShooterSpeed () {
+		
+        if (leftSpeed >= -1) leftSpeed -= .05;
+        if (rightSpeed >= -1) rightSpeed -= .05;
+		
+    }
+    
+    @Override
+    public void periodic() {
+		
+        // This method will be called once per scheduler run
+		
+    }
+    
+    @Override
+    public void initSendable (SendableBuilder builder) {
+		
+        builder.addDoubleProperty("Left Motor Speed", () -> leftSpeed, null);
+        builder.addDoubleProperty("Right Motor Speed", () -> rightSpeed, null);
+		
+    }
+	
 }
