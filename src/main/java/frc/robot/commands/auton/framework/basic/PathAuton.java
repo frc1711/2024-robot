af3e8f6eba0fc1cee652ffd19ba1b1a7fd97cfdf
Trevor Sears <trevor@trevorsears.com>
2024-02-23 12:26:58 -0500

    Fixed indentation and basic formatting in `PathAuton.java`.

--- src/main/java/frc/robot/commands/auton/framework/basic/PathAuton.java
+++ src/main/java/frc/robot/commands/auton/framework/basic/PathAuton.java
@@ -10,29 +10,43 @@ import com.pathplanner.lib.path.PathPlannerPath;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class PathAuton extends Command {
-  
-  PathfindHolonomic pathfind;
-
-  public PathAuton() {
-    pathfind = new PathfindHolonomic(null, null, null, null, null, null, null, null);
-    // Use addRequirements() here to declare subsystem dependencies.
-  }
-
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {}
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+	
+	PathfindHolonomic pathfind;
+	
+	public PathAuton() {
+		
+		pathfind = new PathfindHolonomic(
+			null, 
+			null, 
+			null, 
+			null, 
+			null, 
+			null, 
+			null, 
+			null
+		);
+		// Use addRequirements() here to declare subsystem dependencies.
+		
+	}
+	
+	// Called when the command is initially scheduled.
+	@Override
+	public void initialize() {}
+	
+	// Called every time the scheduler runs while the command is scheduled.
+	@Override
+	public void execute() {}
+	
+	// Called once the command ends or is interrupted.
+	@Override
+	public void end(boolean interrupted) {}
+	
+	// Returns true when the command should end.
+	@Override
+	public boolean isFinished() {
+		
+		return false;
+		
+	}
+	
 }
