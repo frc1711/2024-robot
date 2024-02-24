ce4124fe6160abde4f110263a8b68fe259d45f04
Trevor Sears <trevor@trevorsears.com>
2024-02-24 13:20:07 -0500

    Revised `ArmCommand` formatting and API usage.

--- src/main/java/frc/robot/commands/ArmCommand.java
+++ src/main/java/frc/robot/commands/ArmCommand.java
@@ -10,40 +10,55 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Arm;
 
 public class ArmCommand extends Command {
-  
-  Arm arm;
-
-  BooleanSupplier raiseArm, lowerArm;
-
-  public ArmCommand(Arm arm, BooleanSupplier raiseArm, BooleanSupplier lowerArm) {
-    this.arm = arm;
-    this.raiseArm = raiseArm;
-    this.lowerArm = lowerArm;
-  }
-
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-    arm.stop();
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    if (raiseArm.getAsBoolean()) arm.changeAngle(.5);
-    else if (lowerArm.getAsBoolean()) arm.changeAngle(-.5);
-    else arm.stop();
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    arm.stop();
-  }
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+	
+	protected static final double SPEED = 0.3;
+	
+	protected final Arm arm;
+	
+	protected final BooleanSupplier shouldRaiseArm, shouldLowerArm;
+	
+	public ArmCommand(Arm arm, BooleanSupplier shouldRaiseArm, BooleanSupplier shouldLowerArm) {
+		
+		this.arm = arm;
+		this.shouldRaiseArm = shouldRaiseArm;
+		this.shouldLowerArm = shouldLowerArm;
+		
+		addRequirements(arm);
+		
+	}
+	
+	// Called when the command is initially scheduled.
+	@Override
+	public void initialize() {
+		
+		this.arm.stop();
+		
+	}
+	
+	// Called every time the scheduler runs while the command is scheduled.
+	@Override
+	public void execute() {
+		
+		if (this.shouldRaiseArm.getAsBoolean()) this.arm.rotate(true);
+		else if (this.shouldLowerArm.getAsBoolean()) this.arm.rotate(false);
+		else this.arm.stop();
+		
+	}
+	
+	// Called once the command ends or is interrupted.
+	@Override
+	public void end(boolean interrupted) {
+		
+		this.arm.stop();
+		
+	}
+	
+	// Returns true when the command should end.
+	@Override
+	public boolean isFinished() {
+		
+		return false;
+		
+	}
+	
 }
