79e9aba49c380c54d6f6da88e8323308db4a7911
Trevor Sears <trevor@trevorsears.com>
2024-02-24 13:22:22 -0500

    Added `ControlsTransformsUtilities` helper class.

---
+++ src/main/java/frc/robot/util/ControlsTransformsUtilities.java
@@ -0,0 +1,29 @@
+package frc.robot.util;
+
+import java.util.function.DoubleSupplier;
+import java.util.function.Supplier;
+
+public class ControlsTransformsUtilities {
+
+    public static DoubleSupplier applyDeadband(DoubleSupplier input, double deadband) {
+
+        return () -> ControlsUtilities.applyDeadband(input.getAsDouble(), deadband);
+
+    }
+
+    public static Supplier<Point> applyCircularDeadband(Supplier<Point> input, double deadband) {
+
+        return () -> {
+
+            Point point = input.get();
+
+            double hypotenuse = Math.sqrt(Math.pow(point.x, 2) + Math.pow(point.y, 2));
+
+            if (Math.abs(hypotenuse) < deadband) return point;
+            else return new Point(0, 0);
+
+        };
+
+    }
+
+}
