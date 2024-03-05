2b4399d2ce6ba53b1408e1d4e8bbdd2908ea8bcf
Trevor Sears <trevor@trevorsears.com>
2024-03-05 17:40:30 -0500

    Added swerve module constants for SDS MK4s to `RobotDimensions`.

--- src/main/java/frc/robot/constants/RobotDimensions.java
+++ src/main/java/frc/robot/constants/RobotDimensions.java
@@ -1,7 +1,9 @@
 package frc.robot.constants;
 
+import edu.wpi.first.units.Angle;
 import edu.wpi.first.units.Distance;
 import edu.wpi.first.units.Measure;
+import edu.wpi.first.units.Per;
 
 import static edu.wpi.first.units.Units.*;
 
@@ -30,5 +32,15 @@ public final class RobotDimensions {
 	 * The center distance between the left and right swerve modules.
 	 */
 	public static final Measure<Distance> WIDTHWISE_WHEELBASE = Inches.of(21.25);
+	
+	public static final Measure<Distance> SWERVE_MODULE_WHEEL_DIAMETER = Inches.of(4);
+	
+	public static final double SWERVE_MODULE_DRIVE_MOTOR_RATIO =
+		(50f/14f) * (16f/28f) * (45f/15f);
+	
+	public static final Measure<Per<Distance, Angle>> SWERVE_MODULE_DRIVE_MOTOR_DISTANCE_PER_REVOLUTION =
+		(SWERVE_MODULE_WHEEL_DIAMETER.times(Math.PI)).divide(SWERVE_MODULE_DRIVE_MOTOR_RATIO).per(Rotation);
+	
+	public static final double SWERVE_MODULE_STEERING_MOTOR_RATIO = (1f/12.8f);
 	
 }
