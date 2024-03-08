50b2f7f968136aeabc7b211fb19f1804a0861898
Trevor Sears <trevor@trevorsears.com>
2024-03-08 18:53:42 -0500

    Removed unused `m_testsCommand` field from `Robot`.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -17,7 +17,7 @@ import frc.robot.constants.DoublePreference;
  */
 public class Robot extends TimedRobot {
 	
-    private Command m_autonomousCommand, m_testCommand;
+    private Command m_autonomousCommand;
     
     private RobotContainer m_robotContainer;
     
