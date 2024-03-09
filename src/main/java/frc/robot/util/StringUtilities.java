a5ef004eebba0d4017421bf24eec5c6acecaedb8
Trevor Sears <trevor@trevorsears.com>
2024-03-09 08:29:00 -0500

    Added new `StringUtilities` helper class.

---
+++ src/main/java/frc/robot/util/StringUtilities.java
@@ -0,0 +1,44 @@
+package frc.robot.util;
+
+/**
+ * A collection of utility methods for working with strings.
+ */
+public class StringUtilities {
+	
+	/**
+	 * Returns a new version of the input string with the first letter
+	 * capitalized.
+	 *
+	 * @param string The string to capitalize.
+	 * @return A new version of the input string with the first letter
+	 * capitalized.
+	 */
+	public static String capitalize(String string) {
+		
+		return string.substring(0, 1).toUpperCase() + string.substring(1);
+		
+	}
+	
+	/**
+	 * Returns a new version of the input string with the first letter
+	 * of each word capitalized.
+	 *
+	 * @param string The string to title case.
+	 * @return A new version of the input string with the first letter
+	 * of each word capitalized.
+	 */
+	public static String toTitleCase(String string) {
+		
+		String[] words = string.split(" ");
+		
+		for (int i = 0; i < words.length; i++) {
+			
+			words[i] = words[i].toUpperCase();
+			
+		}
+		
+		return String.join(" ", words);
+		
+	}
+	
+}
